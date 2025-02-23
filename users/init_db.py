35fbc6e3779e23edca04370ed4bf80bea1b4f74c
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-02-23 16:33:03 -0500

    vaciando archivo init se traslado esta funcion a routes.py

--- users/init_db.py
+++
@@ -1,7 +0,0 @@
-from config import app,db
-from models import Users
-
-if __name__=='__main__':
-    with app.app_context():
-        db.create_all()
-        print('La base de datos se creó de forma exitosa 🎉')
