ae46b8645b86f8be557e2c9627ab048ba0d37737
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-02-23 19:31:14 -0500

    agregando función de actulizar datos y función de borrar registros

--- users/routes.py
+++ users/routes.py
@@ -21,7 +21,60 @@ def add_user():
         print(err)
         db.session.rollback()
         return jsonify({'message':'Sorry Internal Server Error'}),500
+@app.route('/get_user',methods=['POST'])
+def get_user():
+    data = request.get_json()
+    email = data['email']
+    password =data['password']
+    if not data or not 'email' in data or not 'password' in data:
+        return jsonify({'message':'Credenciales incorrectas'})
+    try:
+        user= Users.query.filter_by(email=email,password=password).first()
+        return jsonify({'user_info':{'id':user.id,'userName':user.name}})
+
+    except Exception as e:
+        print(f'Error {e}')
+        return jsonify({'message':'Sorry internal server error'})
+@app.route('/update_user/<int:id>',methods=['PATCH'])
+def update_user(id):
+    data = request.get_json()
+    try:
+        user= Users.query.filter_by(id=id).first()
+        if 'password' in data:
+            user.password = data['password']
+            db.session.commit()
+        if 'email' in data:
+            user.email = data['email']
+            db.session.commit()
+        if 'email' in data:
+            user.name = data['userName']
+            db.session.commit()
+
+
+        return jsonify({'user_info':{'userName':user.name,'password':user.password,'email':user.email}})
+    except Exception as e:
+        print(f'Error {e}')
+        return jsonify({'message':'Sorry internal server error'})
+@app.route('/delete_user/<int:id>',methods=['DELETE'])
+def delete_user(id):
     
+    try:
+        user= Users.query.filter_by(id=id).first()
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message':'User deleted succesfull'})
+
+    except Exception as e:
+        print(f'Error {e}')
+        return jsonify({'message':'User not found'})
+    
+
+         
+    
+
+
+
+
 if __name__=='__main__':
     with app.app_context():
         db.create_all()
