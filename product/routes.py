b5ec424021558db768518616f8bf2f779531ac73
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-03-16 19:09:26 -0500

    rehaciendo los mismo ajustes que se hicieron en users y product

--- product/routes.py
+++ product/routes.py
@@ -91,4 +91,6 @@ def delete_product(id):
 if __name__=='__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True,host='0.0.0.0',port=5002)
+        print('base de datos inicializada con éxito'
+        '')
+    app.run(debug=True,host='0.0.0.0',port=5000)
